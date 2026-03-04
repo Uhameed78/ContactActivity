60f3962acfc107a6637ad793514a34c0493d57c0
Uhameed78 <hameedulla.umar@gmail.com>
2026-03-04 17:04:11 +0200

    new

--- app/src/main/java/com/example/mycontats/MainActivity.kt
+++ app/src/main/java/com/example/mycontats/MainActivity.kt
@@ -10,6 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val firstNAME = "Felix"
+        val lastNAME = "Kjellberg"
+        val address = "67 milk street"
+        val isMale = true
+        val dayofbirth = 24
+        val monthofbirth = 10
+        val yearofbirth = 1989
+        println("skadoosh, Top of the mornin' to ya, laddies! It's me,${firstNAME},${lastNAME}, I live at,${address},I am,${isMale},I was born on,${dayofbirth}, ${monthofbirth}, ${yearofbirth},I'm suffocating in the code I wrote, buried beneath broken logic and silent errors, watching the screen glow while my sanity slowly flickers out. insert Brofist here")
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
