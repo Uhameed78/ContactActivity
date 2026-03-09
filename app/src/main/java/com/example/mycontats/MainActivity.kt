ca97329909c7b9e0190c6817754b4b9dde2da248
Uhameed78 <hameedulla.umar@gmail.com>
2026-03-09 08:55:06 +0200

    Shuan

--- app/src/main/java/com/example/mycontats/MainActivity.kt
+++ app/src/main/java/com/example/mycontats/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +18,17 @@ class MainActivity : AppCompatActivity() {
         val dayofbirth = 24
         val monthofbirth = 10
         val yearofbirth = 1989
-        println("skadoosh, Top of the mornin' to ya, laddies! It's me,${firstNAME},${lastNAME}, I live at,${address},I am,${isMale},I was born on,${dayofbirth}, ${monthofbirth}, ${yearofbirth},I'm suffocating in the code I wrote, buried beneath broken logic and silent errors, watching the screen glow while my sanity slowly flickers out. insert Brofist here")
-
+        val currentyear = 2026
+        val age = currentyear - yearofbirth
+        val yearstolive = 100 - age
+        val ageinmonths = age * 12
+        val ageindays = age * 365
+        val remainder = yearstolive % 5
+        Log.d("MainActivity", "remainder: $remainder")
+        Log.d("MainActivity", "yearstoLive: $yearstolive")
+        Log.d("MainActivity", "ageinmonths: $ageinmonths")
+        Log.d("MainActivity", "ageindays: $ageindays")
+        Log.d("MainActivity", "skadoosh, Top of the mornin' to ya, laddies! It's me, $firstNAME $lastNAME, I live at $address, I am male: $isMale, I was born on $dayofbirth/$monthofbirth/$yearofbirth.I'm suffocating in the code I wrote, buried beneath broken logic and silent errors, watching the screen glow while my sanity slowly flickers out. Insert Brofist here")
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
